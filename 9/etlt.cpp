f7df3d04705d794ff99eba57dc67cd325a5d0276
4kangjc <73515066+4kangjc@users.noreply.github.com>
2021-05-05 13:56:03 +0800

    Create etlt.cpp

---
+++ 9/etlt.cpp
@@ -0,0 +1,148 @@
+/**
+ * @file mtlt.cpp
+ * @author kangjinci (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+
+
+const int MAX_EVENT_NUMBER = 1024;
+const int BUFFER_SIZE = 10;
+
+int setnonblocking(int fd) {
+    int old_optition = fcntl(fd, F_GETFL);
+    int new_optition = old_optition | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_optition);
+    return old_optition;
+}
+
+/* 将文件描述符fd上的EPOLLIN注册到epollfd指示的epoll内核事件表中，参数enable_et指定是否对fd启用ET模式*/
+void addfd(int epollfd, int fd, bool enable_et) {
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+    if (enable_et) {
+        event.events |= EPOLLET;
+    }
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+/* LT模式的工作流程 */
+void lt(epoll_event* events, int number, int epollfd, int listenfd) {
+    char buf[BUFFER_SIZE];
+    for (int i = 0; i < number; ++i) {
+        int sockfd = events[i].data.fd;
+        if (sockfd == listenfd) {
+            sockaddr_in client_address;
+            socklen_t client_addrlength = sizeof(client_address);
+            int connfd = accept(listenfd, (sockaddr*)&client_address, &client_addrlength);
+            addfd(epollfd, connfd, false);
+        } else if (events[i].events & EPOLLIN) {
+            printf("event trigger once\n");
+            memset(buf, '\0', BUFFER_SIZE);
+            int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
+            if (ret <= 0) {
+                close(sockfd);
+                continue;
+            }
+            printf("get %d bytes of content: %s\n", ret, buf);
+        } else {
+            printf("something else happended\n");
+        }
+    }
+}
+
+/* ET 模式的工作流程 */
+void et(epoll_event* events, int number, int epollfd, int listenfd) {
+    char buf[BUFFER_SIZE];
+    for (int i = 0; i < number; ++i) {
+        int sockfd = events[i].data.fd;
+        if (sockfd == listenfd) {
+            sockaddr_in client_address;
+            socklen_t client_addrlength = sizeof(client_address);
+            int connfd = accept(listenfd, (sockaddr*)&client_address, &client_addrlength);
+            addfd(epollfd, connfd, true);
+        } else if (events[i].events & EPOLLIN) {
+            printf("event trigger once\n");
+            while (1) {
+                memset(buf, '\0', BUFFER_SIZE);
+                int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
+                if (ret < 0) {
+                    if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+                        printf("read later\n");
+                        break;
+                    }
+                    close(sockfd);
+                    break;
+                } else if (ret == 0) {
+                    close(sockfd);
+                    break;
+                } else {
+                    printf("get %d bytes of content: %s\n", ret, buf);
+                }
+            }
+        } else {
+            printf("something else happended\n");
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc <= 2) {
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int ret = 0;
+    sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &address.sin_addr);
+
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
+
+    ret = bind(listenfd, (sockaddr*)&address, sizeof(address));
+    assert(ret != -1);
+
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(520);
+    assert(epollfd != -1);
+    addfd(epollfd, listenfd, true);
+
+    while (1) {
+        int ret = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if (ret < 0) {
+            printf("epoll failure\n");
+            break;
+        }
+        //lt(events, ret, epollfd, listenfd);
+        et(events, ret, epollfd, listenfd);
+    }
+    close(listenfd);
+    return 0;
+}
